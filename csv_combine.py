0b3e35e7f9daa40b6851fb664f740e548877fbfc
coley22 <jacksoncoley22@gmail.com>
2022-06-14 16:46:50 -0400

    6.14.22 commit

--- csv_combine.py
+++ csv_combine.py
@@ -1,7 +1,19 @@
 import csv
 
+import pandas
+
+rates=[]
 with open('HBEF_lapse_rates.csv') as infile:
     reader = csv.reader(infile)
     for row in reader:
-        # header=next(reader)
-        print(row[1])
+        rat=row.pop(1)
+        rates.append(rat)
+    # print(rates[:4])
+    rates.pop(1)
+    rates.pop(0)
+    print(rates[:4])
+
+df=pandas.read_csv('Histogram_Data.csv')
+df['lapseRate']=rates
+print(df)
+df.to_csv('new_Histogram_Data.csv', index=True)
