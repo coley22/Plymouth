0f0f6787e1abfd424e70d0a702c78609ee9643b0
jacks <jacksoncoley22@gmail.com>
2022-06-21 16:59:05 -0400

    6.21 commit

--- LR_monthly.py
+++ LR_monthly.py
@@ -6,21 +6,21 @@ import matplotlib.pyplot as plot
 df = pd.read_csv('HOBO_Master_40T_LR.csv')
 # print(df)
 # fucking january works now
-df = df[(df['Date'] >= ' 1/0/2013') & (df['Date'] <= '10/0/2019')]
-df.to_csv('january.csv')
-# df = df[(df['Date'] >= ' 9/0/2013') & (df['Date'] <= '10/0/2019')]
+# df = df[(df['Date'] >= ' 1/0/2013') & (df['Date'] <= '10/0/2019')]
+# # df.to_csv('january.csv')
+df = df[(df['Date'] >= '10/0/2013') & (df['Date'] <= '11/0/2020')]
 print(df)
-# df.to_csv('september.csv')
+df.to_csv('october.csv')
 
 # # day
 # df['Date']=pd.to_datetime(df['Date'])
 # df2=df.set_index('Date').between_time('09:00:00','22:00:00')
-# df2.to_csv('july_day.csv')
+# df2.to_csv('january_day.csv')
 #
 # # night
 # df['Date']=pd.to_datetime(df['Date'])
 # df3=df.set_index('Date').between_time('23:00:00','08:00:00')
-# df3.to_csv('july_night.csv')
+# df3.to_csv('january_night.csv')
 #
 # mean=df3['slope'].mean()    # CHANGE AS GRAPH CHANGES
 # mean='{0:.3f}'.format(mean)
@@ -32,10 +32,10 @@ print(df)
 # x=df3['slope']              # CHANGE AS GRAPH CHANGES
 # plot.hist(x, weights=np.ones(len(x))/len(x), bins=bins)
 #
-# plot.title('July night')
+# plot.title('January night')
 # plot.ylabel('relative frequency')
 # plot.xlabel('temperature gradient °C/km')
 # plot.axvline(x=0, color='black', linestyle='--')
 # plot.axvline(x=-9.8, color='red', linestyle='--')
-# plot.text(7.4,.077, mean_string, fontsize=11)
+# plot.text(7.4,.054, mean_string, fontsize=11)
 # plot.show()
--- time_of_Inversion.py
+++ time_of_Inversion.py
@@ -2,22 +2,56 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plot
-df=pd.read_csv('csv/july.csv')
+df=pd.read_csv('csv/january.csv')
 a=df['slope'].values
-u=np.sign(df['slope'])
-m=np.flatnonzero(u.diff().abs().eq(2))
-g=np.stack([m,m],axis=1)
-v=np.abs(a[g].argmin(1))
-df=df.iloc[g[np.arange(g.shape[0]),v]]
-df['Date']=pd.to_datetime(df['Date'])
-df['Time']=df['Date'].dt.hour
-print(df)
+print('a: ',a)  # a is an array with all the slopes
+
+new_df=pd.DataFrame()
+inv_count=0
+i3, i2, i1, i0, i_1, i_2=0,0,0,0,0,0
+for i in range(0,len(a)):
+    i0=a[i]
+    if i==0 or i==1:
+        i3, i2, i1 = a[0], a[0], a[0]
+    elif i==2:
+        i3, i2 = a[i-2], a[i-2]
+        i1 = a[i-1]
+    else:
+        i3=a[i-3]
+        i2=a[i-2]
+        i1=a[i-1]
+        try:
+            i_1=a[i+1]
+            i_2=a[i+2]
+        except:
+            i_1, i_2=a[-1], a[-1]
+
+        if (i3>0 and i2>0 and i1>0 and i0<0) or (i3<0 and i2<0 and i1<0 and i0>0):
+            if (i_1>0 and i_2>0 and i0>0) or (i_1<0 and i_2<0 and i0<0):
+                inv_count+=1
+                print('current count: ',inv_count)
+                print("i3: ",i3)
+                print("i2: ",i2)
+                print("i1: ",i1)
+                print("i0: ",i0,)
+                print("i_1: ",i_1)
+                print("i_2: ",i_2, '\n')
+                new_df=new_df._append(df.iloc[[i]])
+
+
+print('inversion count: ',inv_count)
+new_df.to_csv('january hours with an inversion ND')
+
+new_df['Date']=pd.to_datetime(new_df['Date'])
+new_df['Time']=new_df['Date'].dt.hour
+new_df.drop(new_df.index[new_df['slope']>0], inplace=True)  # dropping slopes with a positive/negative lapse rate
+print(new_df)
 
 # plotting frequency of all inversions in month by hour
-y=df['Time'].value_counts()
-y.sort_index(axis=0, ascending=True, inplace=True)
+y=(new_df['Time'].value_counts()/len(new_df['Time']))
+y.sort_index(axis=0, ascending=True, inplace=True)  # **if an hour has 0 inversions, that hour is left off the x-axis**
 y.plot(kind='bar', color='orange')
-plot.title('Number of inversions by hour (July)')
+plot.title('Number of ** inversions by hour (January)')
 plot.xlabel('time')
-plot.ylabel('# of inversions')
+plot.ylabel('percentage of inversions')
 plot.show()
